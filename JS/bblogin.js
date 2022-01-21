5bd5a99fbfacd8a9c4da9b409da2ef48f28e710f
NayanIngale1 <nayaningale15@gmail.com>
2022-01-21 16:50:33 +0530

    login page created

---
+++ JS/bblogin.js
@@ -0,0 +1,24 @@
+document.querySelector("form").addEventListener("submit", signIn);
+var bbUser = JSON.parse(localStorage.getItem("bbuserDatabase")) || [];
+
+function signIn(event) {
+  event.preventDefault();
+  var user = document.querySelector("#email").value;
+  var pass = document.querySelector("#password").value;
+
+  console.log(bbUser.length);
+
+  var logfail = 0;
+  for (var i = 0; i < bbUser.length; i++) {
+    if (bbUser[i].userEmail == user && bbUser[i].userPass == pass) {
+      window.location.href = "index.html";
+      alert("Use login success");
+    } else {
+      logfail++;
+    }
+  }
+
+  if (logfail == bbUser.length) {
+    alert("User Login Failed! Try Again.");
+  }
+}
