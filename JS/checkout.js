722744705c1cdd3526d351ac7a7fa75614dba0d1
NayanIngale1 <nayaningale15@gmail.com>
2022-01-23 10:24:31 +0530

    All links Are updated.

---
+++ JS/checkout.js
@@ -0,0 +1,177 @@
+document.querySelector("#infoform").addEventListener("submit", savedata);
+
+// var checkoutArray = [];
+function savedata(event) {
+  event.preventDefault();
+  var fname = document.querySelector("#fname1").value;
+  var lname = document.querySelector("#lname1").value;
+  var number = document.querySelector("#number2").value;
+  var House = document.querySelector("#House2").value;
+  var Apartment = document.querySelector("#Apart2").value;
+  var StreetName = document.querySelector("#Street2").value;
+  var landmark = document.querySelector("#LandMark2").value;
+  var city = document.querySelector("#City2").value;
+  var area = document.querySelector("#Area2").value;
+  var pincode = document.querySelector("#Pincode2").value;
+//   if (
+//     fname == "" ||
+//     lname == "" ||
+//     number == "" ||
+//     House == "" ||
+//     Apartment == "" ||
+//     StreetName == "" ||
+//     landmark == "" ||
+//     city == "" ||
+//     area == "" ||
+//     pincode == ""
+//   ) {
+//     var valid = flase;
+//   }
+
+  var checkoutdata = {
+    FName: fname,
+    LName: lname,
+    ContactNo: number,
+    HouseNo: House,
+    Apart: Apartment,
+    Street: StreetName,
+    LandMark: landmark,
+    City: city,
+    Area: area,
+    Pincode: pincode,
+  };
+  // checkoutArray.push(checkoutdata);
+    
+     alert("Your Information is store successfully Thank You!");
+        localStorage.setItem("CheckOutData", JSON.stringify(checkoutdata));
+  // }
+}
+var totalamount = JSON.parse(localStorage.getItem("totalAmount"));
+document.querySelector(
+  "#totalAmount"
+).innerHTML = `: <b>Rs. ${totalamount}</b>`;
+document.querySelector("#applycode").addEventListener("click", applyinput);
+
+var totalPrice = []; //--------You Access Total Amount for Payment Page by "TotalPrice" key----------
+var saveamount = [];
+totalPrice.push(totalamount);
+localStorage.setItem("TotalPrice", JSON.stringify(totalPrice));
+function applyinput() {
+  var inputcode = document.querySelector("#input").value;
+  //console.log(inputcode);
+  if (inputcode == "") {
+    alert("Please Apply PromoCode masai30");
+    return false;
+  }
+  if (inputcode != "masai30") {
+    alert("Please Apply Correct PromoCode");
+    return false;
+  } else {
+    // console.log(1);
+    var reduceamount = totalamount - (totalamount * 30) / 100;
+    // console.log(reduceamount);
+    totalPrice.push(reduceamount);
+    localStorage.setItem("TotalPrice", JSON.stringify(totalPrice));
+    var save = (totalamount * 30) / 100;
+    saveamount.push(save);
+    ShowReduceAmount();
+  }
+}
+function ShowReduceAmount() {
+  var SaveAmountBox = document.createElement("div");
+  SaveAmountBox.setAttribute("class", "SaveAmountBox");
+
+  var YourSavePrice = document.createElement("div");
+  YourSavePrice.setAttribute("class", "YourSavePrice");
+  YourSavePrice.innerHTML = "You Save";
+
+  var ShowSavePrice = document.createElement("div");
+  ShowSavePrice.setAttribute("class", "ShowSavePrice");
+  ShowSavePrice.innerHTML = `: <b>Rs. ${saveamount[saveamount.length - 1]}</b>`;
+
+  document.querySelector("#voucherbox").append(SaveAmountBox);
+  SaveAmountBox.append(YourSavePrice, ShowSavePrice);
+
+  //------------------------------------------------
+  //------------------------------------------------
+  var ReduceAmountBox = document.createElement("div");
+  ReduceAmountBox.setAttribute("class", "SaveAmountBox");
+
+  var ReducePrice = document.createElement("div");
+  ReducePrice.setAttribute("class", "YourSavePrice");
+  ReducePrice.innerHTML = "Total Amount";
+
+  var ShowReducePrice = document.createElement("div");
+  ShowReducePrice.setAttribute("class", "ShowSavePrice");
+  ShowReducePrice.innerHTML = `: <b>Rs. ${
+    totalPrice[totalPrice.length - 1]
+  }</b>`;
+
+  //   document.querySelector("#totalAmount").innerHTML =`Rs. ${totalPrice[totalPrice.length-1]}`;
+  document.querySelector("#voucherbox").append(ReduceAmountBox);
+  ReduceAmountBox.append(ReducePrice, ShowReducePrice);
+
+  document
+    .querySelector("#voucherbox")
+    .append(
+      document.querySelector("#GoToPayment"),
+      document.querySelector("#totalItemBox"),
+      document.querySelector("#ProductShowBox")
+    );
+}
+document.querySelector("#GoToPayment1").addEventListener("click", function () {
+    console.log('heello')
+    var check = JSON.parse(localStorage.getItem("CheckOutData"));
+    var user = checkUser(check);
+    if (user == true) {
+        alert('Thank You!');
+         window.location.href = "payment.html";
+    }
+    else {
+        alert("Please fill The form ..!")
+    }
+     
+    
+   
+   //------------Here You Add Payment File Location
+});
+
+
+function checkUser(check) {
+    console.log(check)
+    if (
+      check.FName == "" ||
+      check.LName == "" ||
+      check.ContactNo == "" ||
+      check.HouseNo == "" ||
+      check.Apart == "" ||
+      check.Street == "" ||
+      check.LandMark == "" ||
+      check.City == "" ||
+      check.Area == "" ||
+      check.Pincode == ""
+    ) {
+      return false;
+    }
+    return true
+}
+
+
+var Product = JSON.parse(localStorage.getItem("cartData"));
+
+document.querySelector(
+  "#totalItemShow"
+).innerHTML = `: ${Product.length} Items`;
+
+Product.map(function (el, index) {
+  var ttl = el.quantity * el.productprice;
+  var para1 = document.createElement("p");
+  para1.setAttribute("class", "totalitems");
+  para1.innerHTML = `<b>${index + 1})</b> ${el.productName} `;
+
+  var para2 = document.createElement("p");
+  para2.setAttribute("class", "totalprc");
+  para2.innerHTML = `<b>Price:</b> Rs. ${ttl}`;
+
+  document.querySelector("#ProductShowBox").append(para1, para2);
+});
--- JS/home.js
+++ JS/home.js
@@ -1,242 +1,245 @@
-console.log("Hello")
+console.log("Hello");
 
-document.querySelector("#read-more-btn").addEventListener('click', function () {
-    document.querySelector("#more-text").style.display = "inline";
-    document.querySelector("#read-more-btn").style.display = "none";
-})
+document.querySelector("#read-more-btn").addEventListener("click", function () {
+  document.querySelector("#more-text").style.display = "inline";
+  document.querySelector("#read-more-btn").style.display = "none";
+});
 
-document.querySelector("#basket-cart").addEventListener("click",function(){
+document.querySelector("#basket-cart").addEventListener("click", function () {
   window.location.href = "basketcart.html";
-})
+});
 
 function newFunction() {
-    $('.carousel').carousel({
-        interval: 1000
-    });
+  $(".carousel").carousel({
+    interval: 1000,
+  });
 }
 
-  var slideBtn = document.getElementsByClassName("slide-btn");
-  var nayan = document.getElementsByClassName("card");
-  var l = 0;
-  slideBtn[1].addEventListener("click", function (e) {
-    l++;
+var slideBtn = document.getElementsByClassName("slide-btn");
+var nayan = document.getElementsByClassName("card");
+var l = 0;
+slideBtn[1].addEventListener("click", function (e) {
+  l++;
 
-    for (var i of nayan) {
-      if (l == 0) {
-        i.style.left = "0px";
-      }
-      if (l == 1) {
-        i.style.left = "-30vw";
-      }
-      if (l == 2) {
-        i.style.left = "-50vwpx";
-      }
-      if (l == 3) {
-        i.style.left = "-70vwpx";
-      }
-      if (l == 4) {
-        i.style.left = "-90vw";
-      }
-      if (l > 4) {
-        l = 4;
-      }
-      if (l == nayan.length ) {
-        l = 0;
+  for (var i of nayan) {
+    if (l == 0) {
+      i.style.left = "0px";
+    }
+    if (l == 1) {
+      i.style.left = "-30vw";
+    }
+    if (l == 2) {
+      i.style.left = "-50vwpx";
+    }
+    if (l == 3) {
+      i.style.left = "-70vwpx";
+    }
+    if (l == 4) {
+      i.style.left = "-90vw";
+    }
+    if (l > 4) {
+      l = 4;
+    }
+    if (l == nayan.length) {
+      l = 0;
+    }
+  }
+});
+
+slideBtn[0].addEventListener("click", function () {
+  l--;
+  for (var i of nayan) {
+    if (l == 0) {
+      i.style.left = "0px";
+    }
+    if (l == 1) {
+      i.style.left = "-30vw";
+    }
+    if (l == 2) {
+      i.style.left = "-50vw";
+    }
+    if (l == 3) {
+      i.style.left = "-90vw";
+    }
+    if (l < 0) {
+      l = 0;
+    }
+  }
+});
+
+var cartArr = JSON.parse(localStorage.getItem("cartData")) || [];
+var FruitVegArr = JSON.parse(localStorage.getItem("vegData"));
+var OneProduct = [];
+
+document.getElementById(
+  "basket-items"
+).innerHTML = `My Basket <br><b> ${cartArr.length} Items.</b>`;
+
+displayProduct(FruitVegArr);
+function displayProduct(FruitVegArr) {
+  document.querySelector("#cardSlider").innerHTML = "";
+
+  FruitVegArr.map(function (element, _index) {
+    var card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    for (var i = 0; i < cartArr.length; i++) {
+      if (element.productName == cartArr[i].productName) {
+        card.style.backgroundColor = "rgba(222, 184, 135, 0.212)";
       }
     }
-  });
 
-  slideBtn[0].addEventListener("click", function () {
-    l--;
-    for (var i of nayan) {
-      if (l == 0) {
-        i.style.left = "0px";
-      }
-      if (l == 1) {
-        i.style.left = "-30vw";
-      }
-      if (l == 2) {
-        i.style.left = "-50vw";
-      }
-      if (l == 3) {
-        i.style.left = "-90vw";
-      }
-      if (l < 0) {
-        l = 0;
-      }
-    }
-  });
+    var discount = document.createElement("div");
+    discount.setAttribute("class", "discount");
 
+    var getOff = document.createElement("p");
+    getOff.setAttribute("class", "get-off");
+    getOff.textContent = `GET ${element.OFF} % OFF`;
 
-    var cartArr = JSON.parse(localStorage.getItem("cartData")) || [];
-    var FruitVegArr = JSON.parse(localStorage.getItem("vegData"))
-    var OneProduct = [];
+    var redDot = document.createElement("div");
+    redDot.setAttribute("class", "red-dot");
 
-    document.getElementById("basket-items").innerHTML = `My Basket <br><b> ${cartArr.length} Items.</b>`
+    var prodimg = document.createElement("div");
+    prodimg.setAttribute("class", "product-photo");
 
+    var prod = document.createElement("img");
+    prod.setAttribute("class", "product-image");
+    prod.setAttribute("src", element.image);
 
-
-  displayProduct(FruitVegArr);
-  function displayProduct(FruitVegArr) {
-    document.querySelector("#cardSlider").innerHTML = "";
-
-    FruitVegArr.map(function (element, _index) {
-      var card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      for (var i = 0; i < cartArr.length; i++) {
-          if (element.productName == cartArr[i].productName) {
-               card.style.backgroundColor = "rgba(222, 184, 135, 0.212)";
-            }
-        }
-
-      var discount = document.createElement("div");
-      discount.setAttribute("class", "discount");
-
-      var getOff = document.createElement("p");
-      getOff.setAttribute("class", "get-off");
-      getOff.textContent = `GET ${element.OFF} % OFF`;
-
-      var redDot = document.createElement("div");
-      redDot.setAttribute("class", "red-dot");
-
-      var prodimg = document.createElement("div");
-      prodimg.setAttribute("class", "product-photo");
-
-      var prod = document.createElement("img");
-      prod.setAttribute("class", "product-image");
-      prod.setAttribute("src", element.image);
-
-      prod.addEventListener("click", function () {
+    prod.addEventListener("click", function () {
       // console.log(element);
       OneProduct.push(element);
       // console.log(OneProduct);
       localStorage.setItem("productDetail", JSON.stringify(OneProduct));
       window.location.href = "ProductDetails.html";
-      });
-
-      var vegBorder = document.createElement("div");
-      vegBorder.setAttribute("class", "veg-tag-border");
-
-      var vegSymbol = document.createElement("div");
-      vegSymbol.setAttribute("class", "veg-color-tag");
-
-      var brand = document.createElement("p");
-      brand.setAttribute("class", "brandName");
-      brand.textContent = element.Brand;
-
-      var prodName = document.createElement("p");
-      prodName.setAttribute("class", "product-name");
-      prodName.textContent = element.productName;
-
-      var note = document.createElement("div");
-      note.textContent = "price for 1Kg";
-      note.setAttribute("class", "note");
-
-      var priceDetail = document.createElement("div");
-      priceDetail.setAttribute("class", "qty-mrp-container");
-
-      var price = document.createElement("div");
-      price.setAttribute("class", "mrp-container");
-
-      var mrpName = document.createElement("p");
-      mrpName.setAttribute("class", "mrp-border");
-      mrpName.textContent = "MRP:";
-
-      var strikePrc = document.createElement("p");
-      strikePrc.setAttribute("class", "strick-price");
-      strikePrc.textContent = "Rs" + element.StrikePrice;
-
-      var prodPrc = document.createElement("p");
-      prodPrc.setAttribute("class", "product-price");
-      prodPrc.textContent = "Rs " + element.productprice;
-
-      var stdDelivery = document.createElement("div");
-      stdDelivery.setAttribute("class", "standart-delivery-box");
-
-      var truckBox = document.createElement("div");
-      truckBox.setAttribute("class", "truckBox");
-
-      var deliveryDate = document.createElement("p");
-      deliveryDate.setAttribute("class", "delivery-date");
-      deliveryDate.textContent = element.Date;
-
-      var truck = document.createElement("img");
-      truck.setAttribute("class", "delivery-truck");
-      truck.setAttribute(
-        "src",
-        "https://www.bbassets.com/static/v2498/custPage/build/content/img/standard-del.svg"
-      );
-
-      var qtyDiv = document.createElement("div");
-      qtyDiv.setAttribute("class", "qty-div");
-
-      var qtyName = document.createElement("p");
-      qtyName.setAttribute("class", "qty-border");
-      qtyName.textContent = "QTY";
-
-      var qtyBox = document.createElement("div");
-      qtyBox.setAttribute("class", "qty-box");
-
-      var takeQty = document.createElement("input");
-      takeQty.setAttribute("class", "qty-input");
-      takeQty.type = "text";
-
-      var addCartDiv = document.createElement("div");
-      addCartDiv.setAttribute("class", "add-to-cart-box");
-
-      var cartBtn = document.createElement("button");
-      cartBtn.textContent = "Add to cart";
-      cartBtn.setAttribute("class", "add-to-cart-button");
-      cartBtn.addEventListener("click", function () {
-        addtocart(takeQty.value, element);
-      });
-
-      qtyBox.append(qtyName, takeQty);
-
-      addCartDiv.append(cartBtn);
-      qtyDiv.append(qtyBox, addCartDiv);
-      truckBox.append(truck);
-      stdDelivery.append(truckBox, deliveryDate);
-      price.append(mrpName, strikePrc, prodPrc);
-      priceDetail.append(price, stdDelivery, qtyDiv);
-      vegBorder.append(vegSymbol);
-      prodimg.append(prod);
-      discount.append(getOff, redDot);
-      card.append(discount, prodimg,vegBorder ,brand,prodName,note,priceDetail);
-      document.querySelector("#cardSlider").append(card);
     });
+
+    var vegBorder = document.createElement("div");
+    vegBorder.setAttribute("class", "veg-tag-border");
+
+    var vegSymbol = document.createElement("div");
+    vegSymbol.setAttribute("class", "veg-color-tag");
+
+    var brand = document.createElement("p");
+    brand.setAttribute("class", "brandName");
+    brand.textContent = element.Brand;
+
+    var prodName = document.createElement("p");
+    prodName.setAttribute("class", "product-name");
+    prodName.textContent = element.productName;
+
+    var note = document.createElement("div");
+    note.textContent = "price for 1Kg";
+    note.setAttribute("class", "note");
+
+    var priceDetail = document.createElement("div");
+    priceDetail.setAttribute("class", "qty-mrp-container");
+
+    var price = document.createElement("div");
+    price.setAttribute("class", "mrp-container");
+
+    var mrpName = document.createElement("p");
+    mrpName.setAttribute("class", "mrp-border");
+    mrpName.textContent = "MRP:";
+
+    var strikePrc = document.createElement("p");
+    strikePrc.setAttribute("class", "strick-price");
+    strikePrc.textContent = "Rs" + element.StrikePrice;
+
+    var prodPrc = document.createElement("p");
+    prodPrc.setAttribute("class", "product-price");
+    prodPrc.textContent = "Rs " + element.productprice;
+
+    var stdDelivery = document.createElement("div");
+    stdDelivery.setAttribute("class", "standart-delivery-box");
+
+    var truckBox = document.createElement("div");
+    truckBox.setAttribute("class", "truckBox");
+
+    var deliveryDate = document.createElement("p");
+    deliveryDate.setAttribute("class", "delivery-date");
+    deliveryDate.textContent = element.Date;
+
+    var truck = document.createElement("img");
+    truck.setAttribute("class", "delivery-truck");
+    truck.setAttribute(
+      "src",
+      "https://www.bbassets.com/static/v2498/custPage/build/content/img/standard-del.svg"
+    );
+
+    var qtyDiv = document.createElement("div");
+    qtyDiv.setAttribute("class", "qty-div");
+
+    var qtyName = document.createElement("p");
+    qtyName.setAttribute("class", "qty-border");
+    qtyName.textContent = "QTY";
+
+    var qtyBox = document.createElement("div");
+    qtyBox.setAttribute("class", "qty-box");
+
+    var takeQty = document.createElement("input");
+    takeQty.setAttribute("class", "qty-input");
+    takeQty.type = "text";
+
+    var addCartDiv = document.createElement("div");
+    addCartDiv.setAttribute("class", "add-to-cart-box");
+
+    var cartBtn = document.createElement("button");
+    cartBtn.textContent = "Add to cart";
+    cartBtn.setAttribute("class", "add-to-cart-button");
+    cartBtn.addEventListener("click", function () {
+      addtocart(takeQty.value, element);
+    });
+
+    qtyBox.append(qtyName, takeQty);
+
+    addCartDiv.append(cartBtn);
+    qtyDiv.append(qtyBox, addCartDiv);
+    truckBox.append(truck);
+    stdDelivery.append(truckBox, deliveryDate);
+    price.append(mrpName, strikePrc, prodPrc);
+    priceDetail.append(price, stdDelivery, qtyDiv);
+    vegBorder.append(vegSymbol);
+    prodimg.append(prod);
+    discount.append(getOff, redDot);
+    card.append(
+      discount,
+      prodimg,
+      vegBorder,
+      brand,
+      prodName,
+      note,
+      priceDetail
+    );
+    document.querySelector("#cardSlider").append(card);
+  });
+}
+
+function addtocart(inputqty, element) {
+  var qty = inputqty;
+  var total = inputqty * element.productprice;
+  // console.log(typeof qty)
+  if (qty == "") {
+    alert("please Enter Some quantity ..");
+    return;
   }
 
-
-
-
-          function addtocart(inputqty, element) {
-            var qty = inputqty;
-            var total = inputqty * element.productprice;
-            // console.log(typeof qty)
-            if(qty == ""){
-                alert("please Enter Some quantity ..");
-                return;
-            }
-            
-            var object1 =
-            {
-                OFF: element.OFF,
-                Brand: element.Brand,
-                Date: element.Date,
-                Link: element.image,
-                StrikePrice: element.StrikePrice,
-                productName: element.productName,
-                productprice: element.productprice,
-                quantity: qty,
-                totalPrice: total,
-            };
-            cartArr.push(object1);
-            window.location.reload();
-            localStorage.setItem("cartData", JSON.stringify(cartArr));
-            // console.log(cart);
-            alert(`Your ${object1.productName} successfully added with ${object1.quantity} quantity. Thank You !`);
-        }
-
-
+  var object1 = {
+    OFF: element.OFF,
+    Brand: element.Brand,
+    Date: element.Date,
+    Link: element.image,
+    StrikePrice: element.StrikePrice,
+    productName: element.productName,
+    productprice: element.productprice,
+    quantity: qty,
+    totalPrice: total,
+  };
+  cartArr.push(object1);
+  window.location.reload();
+  localStorage.setItem("cartData", JSON.stringify(cartArr));
+  // console.log(cart);
+  alert(
+    `Your ${object1.productName} successfully added with ${object1.quantity} quantity. Thank You !`
+  );
+}
